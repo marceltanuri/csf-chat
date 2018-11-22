3c75611d945a216c65c4a5d1a41c6dbbf5aafebe
Marcel Tanuri <marcel.tanuri@gmail.com>
2018-11-21 23:40:10 -0200

    Fix chat position

--- chat.js
+++ chat.js
@@ -85,7 +85,7 @@ if (accountNumber == ""){
 
   $("#cpfPanel").hide();
 
-  ref = "https://chatonline.atendimentocarrefour.com/omni-api/attendance/chat/attendanceIntegration?corporationId=5&channelId=10&queueId=1&fromApi=true&name="+name+"&phone="+finalphone+"&cpf="+cpf+"&email="+email+"&uf=SP";
+  ref = "https://chatonline.atendimentocarrefour.com/omni-api/attendance/chat/attendanceIntegration?corporationId=5&channelId=10&queueId=1&fromApi=true&name="+name+"&cellphone="+finalphone+"&cpf="+cpf+"&email="+email+"&uf=SP";
   openIframe();
 }
 
